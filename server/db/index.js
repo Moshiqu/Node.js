ed35368adf97792599b9d6b3e55964b1e5a0038a
Moshiqu <912323520@qq.com>
2022-11-10 21:36:42 +0800

    数据库密码

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,8 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     user: "root",
-    password: "123456",
+    password: "admin123",
+    // password: "123456",
     database: "test"
 })
 
